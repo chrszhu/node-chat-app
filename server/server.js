b7ba3c79627244b2f4960098ec788e9179801e87
Christopher Zhu <chrszhu@yahoo.com>
2019-01-30 15:21:19 -0800

    heroku fix pls

--- server/server.js
+++ server/server.js
@@ -1,15 +1,12 @@
-console.log(__dirname + '/../public');
-
 const path = require('path');
-const publicPath = path.join(__dirname + '/../public');
 const express = require('express');
-const app = express();
+
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
+var app = express();
+
 app.use(express.static(publicPath));
 
-
-app.listen(3000, () => {
-  console.log(`Listening on port ${port}`);
-})
-
-console.log(publicPath)
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is up on ${port}`);
+});
